baccb2b7d8d1d646f08b16ca0f3b48005fcd2590
Neelmani Bhardwaj <neelmanibhardwaj003@gmail.com>
2025-03-13 18:41:41 -0400

    Adding model files

--- jspHotelManagement/src/main/java/com/hotel/utils/DBConnection.java
+++ jspHotelManagement/src/main/java/com/hotel/utils/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/HotelManagement";
-    private static final String USER = "Justin";
-    private static final String PASSWORD = "12345";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
