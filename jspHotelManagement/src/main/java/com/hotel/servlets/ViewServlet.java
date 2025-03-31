be0a618468a907312065f38f9e8f08afe1add7a1
justin-sies <150500207+justin-sies@users.noreply.github.com>
2025-03-31 06:30:39 -0400

    Add files via upload

---
+++ jspHotelManagement/src/main/java/com/hotel/servlets/ViewServlet.java
@@ -0,0 +1,30 @@
+package com.hotel.servlets;
+
+import com.hotel.dao.ViewDAO;
+import com.hotel.model.*;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ViewServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ViewDAO dao = new ViewDAO();
+
+        List<AvailableRooms> availableRooms = dao.getAvailableRooms();
+        List<RoomCapacity> roomCapacities = dao.getRoomCapacities();
+
+        System.out.println("Available Rooms:" + availableRooms.size());
+        System.out.println("Room Capacities:" + roomCapacities.size());
+
+        request.setAttribute("availableRooms", availableRooms);
+        request.setAttribute("roomCapacities", roomCapacities);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/viewDashboard.jsp");
+        dispatcher.forward(request, response);
+
+    }
+}
+
